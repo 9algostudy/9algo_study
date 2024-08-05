96b55d2413c7befc195c09de57217f537e85ae22
Sungwook Park <respectwo@naver.com>
2024-08-05 17:42:19 +0900

    1253 solved

--- src/윤다은/Main.java
+++ src/윤다은/Main.java
@@ -1,27 +1,54 @@
 package 윤다은;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		Queue<Integer> q = new LinkedList<>();
+		int N = Integer.parseInt(br.readLine());
 		
-		int n = Integer.parseInt(br.readLine());
+		int[] nums = new int[N];
+		int left, right, sum, cnt = 0;
 		
-		for (int i = 1; i <= n; i++) {
-			q.offer(i);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		while (q.size() > 1) {
-			q.poll();
-			q.offer(q.poll());
+		Arrays.sort(nums);
+		
+		for (int i = 0; i < N; i++) {
+			left = 0;
+			right = N-1;
+			
+			while (left < right) {
+				if (left == i) {
+					left++;
+					continue;
+				}
+				if (right == i) {
+					right--;
+					continue;
+				}
+				sum = nums[left] + nums[right];
+				if (sum == nums[i]) {
+					cnt++;
+					break;
+				}
+				
+				if (sum < nums[i]) {
+					left++;
+				}
+				else {
+					right--;
+				}
+			}
 		}
 		
-		System.out.println(q.poll());
+		System.out.println(cnt);
 	}
 }
