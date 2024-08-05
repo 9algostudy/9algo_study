1da38bc607874de135abc83c779afc06fd2efe02
Sungwook Park <respectwo@naver.com>
2024-08-05 17:42:19 +0900

    1253 solved

--- src/윤다은/BOJ_1253_좋은수구하기.java
+++ src/윤다은/BOJ_1253_좋은수구하기.java
@@ -1,5 +1,55 @@
 package 윤다은;
 
-public class BOJ_1253_좋은수구하기 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class BOJ_1253_좋은수구하기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] nums = new int[N];
+		int left, right, sum, cnt = 0;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(nums);
+		
+		for (int i = 0; i < N; i++) {
+			left = 0;
+			right = N-1;
+			
+			while (left < right) {
+				if (left == i) {
+					left++;
+					continue;
+				}
+				if (right == i) {
+					right--;
+					continue;
+				}
+				sum = nums[left] + nums[right];
+				if (sum == nums[i]) {
+					cnt++;
+					break;
+				}
+				
+				if (sum < nums[i]) {
+					left++;
+				}
+				else {
+					right--;
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+	}
 }
