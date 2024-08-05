aada835aa45cd9633eb6406f7b7533536020d793
Sungwook Park <respectwo@naver.com>
2024-08-05 11:18:08 +0900

    update1

--- src/윤다은/BOJ_11720_평균.java
+++ src/윤다은/BOJ_11720_평균.java
@@ -1,7 +1,7 @@
 package 윤다은;
 
 import java.io.*;
-	
+
 public class BOJ_11720_평균 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
