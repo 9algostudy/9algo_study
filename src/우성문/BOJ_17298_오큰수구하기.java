1ce18fefefca467d941906ce4e28361960dd9f49
SSAFY <SSAFY@70.12.103.228>
2024-08-05 14:57:04 +0900

    0805

--- src/우성문/BOJ_17298_오큰수구하기.java
+++ src/우성문/BOJ_17298_오큰수구하기.java
@@ -1,5 +1,12 @@
 package 우성문;
 
-public class BOJ_17298_오큰수구하기 {
+import java.io.*;
 
+public class BOJ_17298_오큰수구하기 {
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("src/우성문/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+	}
 }
