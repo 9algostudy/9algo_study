8401615be4f5175477079d15b9ea52971c22f571
Sungwook Park <respectwo@naver.com>
2024-08-09 17:58:02 +0900

    0809 2343 풀어야함

--- src/윤다은/week2/BOJ_2343_기타레슨.java
+++ src/윤다은/week2/BOJ_2343_기타레슨.java
@@ -1,5 +1,40 @@
 package 윤다은.week2;
 
-public class BOJ_2343_기타레슨 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class BOJ_2343_기타레슨 {
+	static int M;
+	static int min = 0;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		int[] times = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		times[0] = Integer.parseInt(st.nextToken());
+		
+		for (int i = 1; i < N; i++) {
+			times[i] = times[i-1] + Integer.parseInt(st.nextToken());
+		}
+		
+		
+	}
+	
+	static void dfs(int start, int end, int depth, int max) {
+		if (depth == M) {
+			if (min > max) {
+				min = max;
+			}
+			return;
+		}
+		if ()
+	}
 }
