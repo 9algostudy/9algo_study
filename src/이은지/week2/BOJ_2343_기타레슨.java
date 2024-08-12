5276c4ac4f43dcc981bde3934a0868e39825c1c9
eenzzi <purelej99@gmail.com>
2024-08-12 22:06:38 +0900

    2주차 시작

--- src/이은지/week2/BOJ_2343_기타레슨.java
+++ src/이은지/week2/BOJ_2343_기타레슨.java
@@ -1,5 +1,29 @@
 package 이은지.week2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_2343_기타레슨 {
 
+    static int N, M;
+    static int[] lecture;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        lecture = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            lecture[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+    }
 }
