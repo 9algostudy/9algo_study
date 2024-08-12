baf6d78ba6eb99aafb6f58ca53711c5f73e236d8
SSAFY <SSAFY@70.12.103.228>
2024-08-12 17:50:47 +0900

    0812

--- src/우성문/week2/BOJ_13023_ABCDE.java
+++ src/우성문/week2/BOJ_13023_ABCDE.java
@@ -1,5 +1,28 @@
 package 우성문.week2;
 
+import java.io.*;
+import java.util.*;
+
 public class BOJ_13023_ABCDE {
 
+	public static void main(String[] args) throws Exception {
+			System.setIn(new FileInputStream("src/우성문/week2/input.txt"));
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+			StringBuilder sb = new StringBuilder(1000);
+
+			String[] line = br.readLine().trim().split(" ");
+			
+			int n = Integer.parseInt(line[0]);
+			int m = Integer.parseInt(line[1]);
+			
+
+
+
+			bw.write(sb.toString());
+			bw.flush();
+			bw.close();
+
+		}
 }
