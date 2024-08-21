e3fd103426f944efed4c8107ee23ff1f559d5504
Daeun <syoon4486@gmail.com>
2024-08-21 19:13:18 +0900

    연구실 주석 추

--- src/윤다은/week3plus/BOJ_14502_연구실.java
+++ src/윤다은/week3plus/BOJ_14502_연구실.java
@@ -24,21 +24,27 @@ public class BOJ_14502_연구실 {
 		
 		map = new int[N][M];
 		
+		//map 저장
 		for (int i = 0; i < N; i++) {
 			s = br.readLine().split(" ");
 			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(s[j]);
+				//기존에 바이러스 없는 공간 개수 세기
 				if (map[i][j] == 0) cleanCnt++;
+				//바이러스면 ArrayList에 추가
 				else if (map[i][j] == 2) virus.add(new Point(i, j));
 			}
 		}
-		cleanCnt -= 3;		
+		cleanCnt -= 3;	
+		//벽 세우는 부분. 브루트포스 알고리즘
 		dfs(0);
+		//기존에 바이러스 없는 공간에서 최소로 바이러스 퍼트린 수 구하기
 		System.out.println(cleanCnt-min);
 	}
 
 	static void dfs(int n) {
 		if (n == 3) {
+			//벽 세개 다 세우면 바이러스 퍼트리기
 			bfs();
 			return;
 		}
@@ -57,16 +63,22 @@ public class BOJ_14502_연구실 {
 	static void bfs() {
 		Queue<Point> queue = new LinkedList<Point>();
 		Point p;
+		
+		//맵 copy
 		int[][] tmp = new int[N][M];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
 				tmp[i][j] = map[i][j];
 			}
 		}
+		
+		//queue에 바이러스 넣기
 		int cnt = 0;
 		for (int i = 0; i < virus.size(); i++) {
 			queue.add(virus.get(i));
 		}
+		
+		//queue가 빌 때까지 바이러스 퍼트리기. 퍼트린 개수 센다
 		while (!queue.isEmpty()) {
 			p = queue.poll();
 			for (int j = 0; j < 4; j++) {
@@ -78,6 +90,7 @@ public class BOJ_14502_연구실 {
 				queue.add(new Point(newx, newy));
 			}
 		}
+		//퍼트린 바이러스의 최소값 구하기
 		if (min > cnt) min = cnt;
 	}
 }
