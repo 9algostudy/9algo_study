45905165b3e1d1dcc54483157f20f4b8f76df4e6
a <a>
2024-08-29 17:47:58 +0900

    0829 회전초밥 수정

--- src/윤다은/week4/BOJ_15961_회전초밥.java
+++ src/윤다은/week4/BOJ_15961_회전초밥.java
@@ -12,17 +12,15 @@ public class BOJ_15961_회전초밥 {
 		int plates = Integer.parseInt(s[0]);
 		int num = Integer.parseInt(s[2]);
 		int coupon = Integer.parseInt(s[3]);
-		int service = 1;
 		
 		Map<Integer, Integer> sushi = new HashMap<>();
 		Queue<Integer> queue = new LinkedList<Integer>();
 		Queue<Integer> queue2 = new LinkedList<Integer>();
 		
 		
-//		sushi.put(coupon, 1);
+		sushi.put(coupon, 1);
 		for (int i = 0; i < num; i++) {
 			int n = Integer.parseInt(br.readLine());
-			if (n == coupon) service = 0;
 			if (sushi.get(n) == null) {
 				sushi.put(n, 1);
 			} else {
@@ -32,7 +30,7 @@ public class BOJ_15961_회전초밥 {
 			queue2.offer(n);
 		}
 
-		int max = queue.size();
+		int max = sushi.size();
 		
 		int n, size;
 		for (int i = 0; i < plates-num; i++) {
@@ -40,11 +38,8 @@ public class BOJ_15961_회전초밥 {
 			size = sushi.remove(n);
 			if (size > 1)
 				sushi.put(n, size-1);
-			else 
-				if (n == coupon) service = 1;
 			
 			n = Integer.parseInt(br.readLine());
-			if (n == coupon) service = 0;
 			if (sushi.get(n) == null) {
 				sushi.put(n, 1);
 			} else {
@@ -52,7 +47,7 @@ public class BOJ_15961_회전초밥 {
 			}
 			
 			queue.offer(n);
-			max = Math.max(max, sushi.size() + service);
+			max = Math.max(max, sushi.size());
 		}
 		
 		for (int i = 0; i < num-1; i++) {
@@ -60,11 +55,8 @@ public class BOJ_15961_회전초밥 {
 			size = sushi.remove(n);
 			if (size > 1)
 				sushi.put(n, size-1);
-			else
-				if (n == coupon) service = 1;
 			
 			n = queue2.poll();
-			if (n == coupon) service = 0;
 			if (sushi.get(n) == null) {
 				sushi.put(n, 1);
 			} else {
@@ -72,7 +64,7 @@ public class BOJ_15961_회전초밥 {
 			}
 			
 			queue.offer(n);
-			max = Math.max(max, sushi.size() + service);
+			max = Math.max(max, sushi.size());
 		}
 		
 		System.out.println(max);
