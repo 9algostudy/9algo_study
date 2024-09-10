de4f6d52bc4d6657f1d6a23064dc95b3c3c4c40b
respectwo <respectwo@gmail.com>
2024-09-10 09:48:50 +0900

    0910 commit 기타레슨 최소비용 줄세우기 치킨배달

--- src/윤다은/week5/BOJ_1261_알고스팟.java
+++ src/윤다은/week5/BOJ_1261_알고스팟.java
@@ -1,5 +1,11 @@
 package 윤다은.week5;
 
-public class BOJ_1261_알고스팟 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class BOJ_1261_알고스팟 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
 }
