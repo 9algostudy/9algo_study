e7c4e1e9778b066a41b8d8e96ca44d96523fc546
shinseongwoo <ssw4510@gmail.com>
2024-09-10 09:48:50 +0900

    0910 commit 기타레슨 최소비용 줄세우기 치킨배달

--- src/윤다은/week2/BOJ_2343_기타레슨.java
+++ src/윤다은/week2/BOJ_2343_기타레슨.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_2343_기타레슨 {
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,68 +13,40 @@ public class BOJ_2343_기타레슨 {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		int[] times = new int[N];
-		int[] sums = new int[M];
-		int[] pointer = new int[M-1];
-		
+		int[] blueray = new int[N];
+		int totalLen = 0, max = 1;
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			times[i] = Integer.parseInt(st.nextToken());
-			sums[i*M/N] += times[i];
+			blueray[i] = Integer.parseInt(st.nextToken());
+			totalLen += blueray[i];
+			max = Math.max(max, blueray[i]);
 		}
 		
-		for (int i = 0; i < M-1; i++) {
-			pointer[i] = N/M*(i+1);
-		}
-		
-		int min = sums[0], max = sums[0], cur = 0;
-		int minIndex = 0, maxIndex = 0;
-		
-		for (int i = 1; i < M; i++) {
-			if (min > sums[i]) {
-				min = sums[i];
-				minIndex = i;
-			} else if (max < sums[i]){
-				max = sums[i];
-				maxIndex = i;
-			}
-		}
-		
-		do {
-			cur = max;
+		//left를 1로 설정하면 틀린다. 무조건 최소값 잘 생각해서 해줘야함
+		int left = max, right = totalLen;
+		while (left < right) {
+			int mid = (left+right)/2;
 			
-			if (minIndex < maxIndex) {
-				for (int i = maxIndex-1; i >= minIndex; i--) {
-					sums[i+1] -= times[pointer[i]];
-					sums[i] += times[pointer[i]++];
-					if (sums[i] < max) {
+			int cnt = 1, len = 0;
+			boolean pos = true;
+			for (int i = 0; i < N; i++) {
+				len += blueray[i];
+				if (len > mid) {
+					cnt++;
+					len = blueray[i];
+					if (cnt > M) {
+						pos = false;
 						break;
 					}
 				}
+			}
+			if (pos == false) {
+				left = mid+1;
 			} else {
-				for (int i = maxIndex; i > minIndex; i++) {
-					sums[i] -= times[pointer[i]];
-					sums[i+1] += times[pointer[i]++];
-					if (sums[i+1] < max) {
-						break;
-					}
-				}
+				right = mid;
 			}
-			
-			min = sums[0];
-			max = sums[0];
-			for (int i = 1; i < M; i++) {
-				if (min > sums[i]) {
-					min = sums[i];
-					minIndex = i;
-				} else if (max < sums[i]){
-					max = sums[i];
-					maxIndex = i;
-				}
-			}
-			
-		} while (cur > max);
+		}
 		
-		System.out.println(cur);
+		System.out.println(right);
 	}
 }
