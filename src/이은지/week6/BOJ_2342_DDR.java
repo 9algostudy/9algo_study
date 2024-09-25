979e6716fb0bb614b5886ec098cdcd626258ec42
eenzzi <purelej99@gmail.com>
2024-09-25 17:55:20 +0900

    6주차

--- src/이은지/week6/BOJ_2342_DDR.java
+++ src/이은지/week6/BOJ_2342_DDR.java
@@ -3,33 +3,83 @@ package 이은지.week6;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class BOJ_2342_DDR{
 
-    static int[] now;
-    static int[][] dp;
+    static int size;
+    static int[][][] dp;
+    static ArrayList<Integer> list;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        list = new ArrayList<>();
+        list.add(0);
 
-        now = new int[2]; //왼, 오
-        dp = new int[5][5];
+        while (true){
+            int i = Integer.parseInt(st.nextToken());
 
-        for (String s : input.split("")) {
-            if (s.equals("0")) {
+            if (i == 0) {
                 break;
             }
 
-            int i = Integer.parseInt(s);
+            list.add(i);
+        }
 
-            command(i);
+        size = list.size();
+        dp = new int[size][5][5]; // index, 현재 왼쪽, 현재 오른쪽
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    dp[i][j][k] = 987654321;
+                }
+            }
 
         }
+
+        dp[0][0][0] = 0;
+        int min = 987654321;
+
+        for (int idx = 0; idx < size - 1; idx++) {
+            int end = list.get(idx + 1); // 다음 타겟
+
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    int now = dp[idx][i][j];
+
+                    if (end != j) { // 두발이 동일하지 않도록 => 왼발 움직이는 경우
+                        dp[idx + 1][end][j] = Math.min(dp[idx + 1][end][j], now + cost(i, end));
+                    }
+
+                    if (end != i) { // 두발이 동일하지 않도록 => 오른발 움직이는 경우
+                        dp[idx + 1][i][end] = Math.min(dp[idx + 1][i][end], now + cost(j, end));
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                min = Math.min(min, dp[size - 1][i][j]);
+            }
+        }
+
+        System.out.println(min);
     }
 
-    private static void command(int n) {
-
+    private static int cost(int start, int end) {
+        if (start == end) {
+            return 1;
+        } else if (start == 0) {
+            return 2;
+        } else if (Math.abs(start - end) == 1 || Math.abs(start - end) == 3) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 
 }
