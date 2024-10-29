27255d9f5ba72f6e3c67ea23a16f3f2a8fa233ef
Daeun <syoon4486@gmail.com>
2024-10-29 22:56:21 +0900

    commit 이중우선순위

--- src/윤다은/week10/BOJ_7662_이중우선순위큐.java
+++ src/윤다은/week10/BOJ_7662_이중우선순위큐.java
@@ -3,7 +3,7 @@ package 윤다은.week10;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 
@@ -17,14 +17,15 @@ public class BOJ_7662_이중우선순위큐 {
 		while (N-- > 0) {
 			int T = Integer.parseInt(br.readLine());
 
-			PriorityQueue<Integer> maxq = new PriorityQueue<>(new Comparator<Integer>() {
-
-				@Override
-				public int compare(Integer o1, Integer o2) {
-					return o2 - o1;
-				}
-
-			});
+//			PriorityQueue<Integer> maxq = new PriorityQueue<>(new Comparator<Integer>() {
+//
+//				@Override
+//				public int compare(Integer o1, Integer o2) {
+//					return o2 - o1;
+//				}
+//
+//			});
+			PriorityQueue<Integer> maxq = new PriorityQueue<>(Collections.reverseOrder());
 
 			PriorityQueue<Integer> minq = new PriorityQueue<>();
 
@@ -36,42 +37,60 @@ public class BOJ_7662_이중우선순위큐 {
 				int n = Integer.parseInt(s[1]);
 
 				if (s[0].equals("I")) {
-					if (maxq.contains(n)) {
-						hm.put(n, hm.get(n) + 1);
-					} else {
+						hm.put(n, hm.getOrDefault(n, 0) + 1);
 						maxq.offer(n);
 						minq.offer(n);
-						hm.put(n, 1);
-					}
-				} else if (s[0].equals("D")) {
-					if (!minq.isEmpty()) {
-						if (n == 1) {
-							int a = maxq.peek();
-							int b = hm.get(a);
+				} else {
+					if (hm.size() == 0)
+						continue;
+					if (n == 1) {
+						int a;
+						while (true) {
+							a = maxq.poll();
+							int b = hm.getOrDefault(a, 0);
+							if (b == 0)
+								continue;
 							if (b == 1) {
 								hm.remove(a);
-								minq.remove(a);
-								maxq.poll();
+							} else {
+								hm.put(a, b - 1);
 							}
-							else {
-								hm.put(a, b-1);
-							}
-						} else if (n == -1) {
-							int a = minq.peek();
-							int b = hm.get(a);
+							break;
+						}
+					} else {
+						int a;
+						while (true) {
+							a = minq.poll();
+							int b = hm.getOrDefault(a, 0);
+							if (b == 0)
+								continue;
 							if (b == 1) {
 								hm.remove(a);
-								maxq.remove(a);
-								minq.poll();
+							} else {
+								hm.put(a, b - 1);
 							}
-							else {
-								hm.put(a, b-1);
-							}
+							break;
 						}
 					}
 				}
 			}
-			System.out.println(minq.isEmpty() ? "EMPTY" : maxq.poll() + " " + minq.poll());
+			if (hm.size() == 0) {
+				System.out.println("EMPTY");
+			} else {
+				int max=0, min=0;
+				while (true) {
+					max = maxq.poll();
+					if (hm.getOrDefault(max, 0) == 0) continue;
+					break;
+				}
+				while (true) {
+					min = minq.poll();
+					if (hm.getOrDefault(min, 0) == 0) continue;
+					break;
+				}
+				System.out.println(max + " " + min);
+			}
+			
 		}
 	}
 }
