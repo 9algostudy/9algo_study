f1782b9ed6ed2c098d759ad200682c2e7e9a5823
eenzzi <purelej99@gmail.com>
2024-10-30 17:59:30 +0900

    10주차

---
+++ src/이은지/week10/BOJ_2156_포도주시식.java
@@ -0,0 +1,14 @@
+package 이은지.week10;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_2156_포도주시식 {
+
+	public static void main(String[] args) {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	}
+
+}
