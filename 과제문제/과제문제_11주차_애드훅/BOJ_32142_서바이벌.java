b00214ce7e86155f253b542c6f88ce202fedf2aa
respectwo2 <respectwo@gmail.com>
2024-11-03 18:19:54 +0900

    add: problem upload

---
+++ 과제문제/과제문제_11주차_애드훅/BOJ_32142_서바이벌.java
@@ -0,0 +1,11 @@
+package 과제문제_11주차_애드훅;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_32142_서바이벌 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
+
---
+++ 과제문제/과제문제_11주차_애드훅/BOJ_32299_게임을_만들어요.java
@@ -0,0 +1,11 @@
+package 과제문제_11주차_애드훅;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_32299_게임을_만들어요 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
+
