05fd8ed4a5f63dcc25b517f568a61e7ec391477d
sungwook <respectwo@naver.com>
2024-11-05 10:56:19 +0900

    add: 11주차 문제 변경 및 업로드

--- 과제문제/과제문제_11주차_A형기출/BOJ_17406_배열돌리기4.java
+++ 과제문제/과제문제_11주차_A형기출/BOJ_17406_배열돌리기4.java
@@ -1,10 +1,10 @@
-package 과제문제_11주차_애드훅;
+package 과제문제_11주차_A형기출;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class BOJ_11565_바이너리게임 {
-	public static void main(String[] args) {
+public class BOJ_17406_배열돌리기4 {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}
 }
