5c61fcf178f661a9f17f3db41520a0157b9afe59
sungwook <respectwo@naver.com>
2024-11-05 10:57:17 +0900

    add: 11주차 문제 업로드

---
+++ src/박성욱/week11/BOJ_17070_파이프옮기기1.java
@@ -0,0 +1,11 @@
+package 박성욱.week11;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_17070_파이프옮기기1 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
+
