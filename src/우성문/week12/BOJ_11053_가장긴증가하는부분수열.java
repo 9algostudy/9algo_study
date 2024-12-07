36f257b7bbc5a5788f675694786512eefa2008b9
우성문 <tjdans7665@gmail.com>
2024-12-07 15:34:05 +0900

    solve:2096

---
+++ src/우성문/week12/BOJ_11053_가장긴증가하는부분수열.java
@@ -0,0 +1,8 @@
+package 우성문.week12;
+
+public class BOJ_11053_가장긴증가하는부분수열 {
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/우성문/week12/BOJ_14940_쉬운최단거리.java
@@ -0,0 +1,9 @@
+package 우성문.week12;
+
+public class BOJ_14940_쉬운최단거리 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
