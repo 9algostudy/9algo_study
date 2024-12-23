ba2fa1cc8358673df7f5bda23bc4ee68b966c599
respectwo2 <respectwo@gmail.com>
2024-12-23 16:29:28 +0900

    add: 14주차 문제 업로드

--- 과제문제/과제문제_14주차/BOJ_14502_연구소.java
+++ 과제문제/과제문제_14주차/BOJ_14502_연구소.java
@@ -1,8 +1,11 @@
 package 과제문제_14주차;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class BOJ_14502_연구소 {
 	public static void main(String[] args) throws Exception {
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}
 
 }
